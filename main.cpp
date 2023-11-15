450d088fe2d51355cec7a68d9a592490d92b1a57
Bnn16 <Bogdan.Nikolov4@outlook.com>
2023-11-15 12:42:17 +0100

    section 1.4

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+void funcWhile(){
+    std::cout<<"While loop 50 to 100" << std::endl;
+    int i = 5;
+    while(i <= 10) {
+        std::cout << i << std::endl;
+        i++;
+    }
+}
+
+void funcWhileMinus(){
+    std::cout << "While loop 5 to 0" << std::endl;
+    int i = 5;
+    while(i >= 0){
+        std::cout << i << std::endl;
+        i--;
+    }
+}
 
 int main() {
-    std::cout << "Hello I like russian ladies" << std::endl;
-    std::cout<<"Enter numbers" << std::endl;
-    int v1= 0, v2= 0;
+    funcWhile();
+    funcWhileMinus();
+
+    int v1 = 0;
+    int v2 = 0;
     std::cin >> v1 >> v2;
-    int sum = v1 + v2;
-    std::cout << "The sum is " << sum << std::endl;
-    int multiSum = v1 * v2;
-    std::cout << "The multiplication sum is " << multiSum << std::endl;
+    if(v1 > v2){
+        std::cout << "Please make v1 less than v2" << std::endl;
+        std::cin >> v1;
+    }
+    while(v1 <= v2){
+        std::cout << v1 << std::endl;
+        v1++;
+    }
     return 0;
 }
