d2da3fe0456f4a4e6876ebf38a484ec76d6d80ac
Bnn16 <Bogdan.Nikolov4@outlook.com>
2023-11-15 11:43:51 +0100

    section 1.2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 int main() {
+    std::cout << "Hello I like russian ladies" << std::endl;
     std::cout<<"Enter numbers" << std::endl;
     int v1= 0, v2= 0;
-    std::cin>> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    std::cin >> v1 >> v2;
+    int sum = v1 + v2;
+    std::cout << "The sum is " << sum << std::endl;
+    int multiSum = v1 * v2;
+    std::cout << "The multiplication sum is " << multiSum << std::endl;
     return 0;
 }
